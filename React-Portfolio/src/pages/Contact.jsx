32dc61cb2b006166bc1bcae3da7216532e94e14d
Wgomez1987 <gomez.william.a1@gmail.com>
2023-11-17 10:04:45 -0800

    final commit

--- React-Portfolio/src/pages/Contact.jsx
+++ React-Portfolio/src/pages/Contact.jsx
@@ -2,18 +2,11 @@ export default function Contact() {
     return (
       <div>
         <h1>Contact Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        <p className="color-red">
+       For any inquiries, please feel free to reach out to me at:
+       <a className="contact"href="mailto:gomez.william.a1@gmail.com">Email</a>
+       <a className="contact"href="https://github.com/Wgomez1987">Github</a>
+       <a className="contact"href="https://www.linkedin.com/">Linkedin</a>
         </p>
       </div>
     );
---
+++ React-Portfolio/src/pages/Project.jsx
@@ -0,0 +1,12 @@
+export default function Project() {
+    return (
+      <div>
+        <h1>Projects Page</h1>
+        <div>
+          pic of projcets Here
+        </div>
+        
+      </div>
+    );
+  }
+  
