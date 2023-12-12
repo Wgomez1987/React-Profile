97b9ee0fa51fcb50ad07d9152a4dbfdc4aec1fb7
Wgomez1987 <gomez.william.a1@gmail.com>
2023-12-12 15:12:38 -0800

    committing from other machine

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,13 @@
 export default function About() {
     return (
-      <div>
+      <div className='about d-flex justify-content-center flex-column text-center'>
         <h1>About Page</h1>
+        <div className='paragraph1'>
         <p>
          About paragraph here.
         </p>
+        </div>
+       
       </div>
     );
   }
