d72f756eb4ecfda5860dcd08b8d78fbdf66ae8f2
Wgomez1987 <gomez.william.a1@gmail.com>
2024-01-05 14:58:13 -0800

    added about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,17 @@
 export default function About() {
   return (
     <div className='about d-flex justify-content-center flex-column text-center'>
-      <h1>About Page</h1>
+      <h1>About:</h1>
       <div className='paragraph1'>
         <div id='my-image'>
           <a><img src='./public/IMG_0871.PNG' width='150' height='250'></img></a>
         </div>
         <p>
-          About paragraph here.
+          Welcome to my portfolio!  My name is William Gomez, and I have recently graduated from UCLA's Bootcamp Extension as a Full Stack Developer.  I decided to take on a new career path after almost 20 years in the Grocery Industry.  In the Grocery Industry, I worked anywhere you can think of.  I started at a big chain supermarket in 2007 as a bag boy.  I worked in every department including Produce, Dairy/Deli, Night Crew, Cashier, and Receiver to name a few.  After 10 years at the large chain, I was awarded an opportunity to transistion into a Natural Foods Grocer where there was a little more finesse and nietzsche items.  Luckily I did not have to start at the bottom as my 10 years prior experience allowed me to get fast-tracked into Management almost immediately.  I was a Front-End Manager for three years before I was asked to take on the role of a Store Director.  I jumped at the opportunity and for the next six years, I ran a store in the very posh neighborhood of Los Feliz, in Los Angeles. There were many ups-and-downs, but I ultimately got my berrings and ran a pretty successful store for six years.<br/>
+
+          <br/>After years of heavy lifting, long hours, and rare weekends/holidays off, my wife and I decided that it would be time to try something new.  I have been interested in Software Development for a long time, but never really had any time to pursue it.  Finally, I had my chance!  I am now proud to say that I have completed UCLA's Bootcamp Extension and am looking forward to working in the industry.  Please feel free to contact me, and view my projects page!  Thank you for your time, and I hope you enjoy.  
+
+          P.S. I built everything you are scrolling through, and buttons you are pressing.  Take care!
         </p>
       </div>
 
