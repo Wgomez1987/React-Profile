a3a1d830d1f8aa7cc39ff2541aa4da7ee94de4a0
Wgomez1987 <gomez.william.a1@gmail.com>
2024-01-05 15:00:28 -0800

    fixed img issue for netlify

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <h1>About:</h1>
       <div className='paragraph1'>
         <div id='my-image'>
-          <a><img src='./public/IMG_0871.PNG' width='150' height='250'></img></a>
+          <a><img src='./IMG_0871.PNG' width='150' height='250'></img></a>
         </div>
         <p>
           Welcome to my portfolio!  My name is William Gomez, and I have recently graduated from UCLA's Bootcamp Extension as a Full Stack Developer.  I decided to take on a new career path after almost 20 years in the Grocery Industry.  In the Grocery Industry, I worked anywhere you can think of.  I started at a big chain supermarket in 2007 as a bag boy.  I worked in every department including Produce, Dairy/Deli, Night Crew, Cashier, and Receiver to name a few.  After 10 years at the large chain, I was awarded an opportunity to transistion into a Natural Foods Grocer where there was a little more finesse and nietzsche items.  Luckily I did not have to start at the bottom as my 10 years prior experience allowed me to get fast-tracked into Management almost immediately.  I was a Front-End Manager for three years before I was asked to take on the role of a Store Director.  I jumped at the opportunity and for the next six years, I ran a store in the very posh neighborhood of Los Feliz, in Los Angeles. There were many ups-and-downs, but I ultimately got my berrings and ran a pretty successful store for six years.<br/>
