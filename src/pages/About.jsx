44c96ebb483f79c49a9f25f28a87f6b7cfc0467b
Wgomez1987 <gomez.william.a1@gmail.com>
2024-01-05 14:41:33 -0800

    picture in about added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,16 @@
 export default function About() {
-    return (
-      <div className='about d-flex justify-content-center flex-column text-center'>
-        <h1>About Page</h1>
-        <div className='paragraph1'>
-        <p>
-         About paragraph here.
-        </p>
+  return (
+    <div className='about d-flex justify-content-center flex-column text-center'>
+      <h1>About Page</h1>
+      <div className='paragraph1'>
+        <div id='my-image'>
+          <a><img src='./public/IMG_0871.PNG' width='150' height='250'></img></a>
         </div>
-       
+        <p>
+          About paragraph here.
+        </p>
       </div>
-    );
-  }
-  
+
+    </div>
+  );
+}
